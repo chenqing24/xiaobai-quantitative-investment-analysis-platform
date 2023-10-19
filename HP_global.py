c470224f0c1c79e9f2bec60dc9c92545d798eca5
chenqing <chenqing24@163.com>
2023-10-19 14:39:28 +0800

    add hp_global

---
+++ HP_global.py
@@ -0,0 +1,41 @@
+'''
+ # @ Author: Jeff Chen
+ # @ Create Time: 2023-10-19 14:17:08
+ # @ Description: 全局变量定义
+ '''
+
+# 运行环境
+# os=1 win, =2 linux, =3 mac
+global os
+global pyver # py版本
+
+# 软件相关
+global name
+global title
+global developer
+global ver
+
+# 数据
+global datapath
+global prgpath # 程序目录
+
+# 展示主窗口
+global winW # 默认宽度
+global winH # 高度
+global root # 窗口根句柄
+global ico # 图标
+
+# 用户
+global user
+global login # 登录标记
+
+# 绘图
+global cns # 颜色表
+global hcolor # 高亮颜色
+global hcursor # 鼠标模式
+global ubg # 背景色
+global ufg # 前景色
+global utg # 文字颜色
+global uvg # 成交量颜色
+
+# TODO 控件名
